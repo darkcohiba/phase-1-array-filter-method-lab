1d2444b19255458766ea5687c3866446157a101e
Sam <samuel.waters@gmail.com>
2022-02-26 11:21:04 -0700

    commit tree 4825c2421ed57370a4dd127125797cbd2d73ad53

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+];
+
+function findMatching(array, str){
+    if (str == array.name){
+        return array.name;
+    }
+}
+
+drivers.filter(findMatching(drivers, 'Bobby'))
+
+function fuzzyMatch(array, str){
+    if (str === array.name.startsWith(str)){
+        return array.name;
+    }
+}
